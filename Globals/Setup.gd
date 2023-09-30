93e9977a9e6865058cb0108b949ec73ddc01c910
Viktor-Domenstam <56826979+Viktor-Domenstam@users.noreply.github.com>
2023-10-01 00:25:06 +0200

    Fixed load event function
    Fixed load event function

--- Globals/Setup.gd
+++ Globals/Setup.gd
@@ -43,17 +43,75 @@ func load_event():
 	GlobalEvent.prompt = event["Prompt"]
 	
 	#Load choices
-	for i in 4:
-		GlobalEvent.choice_one.choice_prompt = event["Choices"][i]["ChoicePrompt"]
-		GlobalEvent.choice_one.choice_type = event["Choices"][i]["ChoiceType"]
-		GlobalEvent.choice_one.increase_type = event["Choices"][i]["IncreaseType"]
-		GlobalEvent.choice_one.decrease_type = event["Choices"][i]["DecreaseType"]
-		GlobalEvent.choice_one.success_chance = event["Choices"][i]["SuccessChance"]
-		GlobalEvent.choice_one.skill_modifier = event["Choices"][i]["SkillModifier"]
-		GlobalEvent.choice_one.outcome_positive_prompt = event["Choices"][i]["Outcome"][0]["Prompt"]
-		GlobalEvent.choice_one.outcome_positive_increase_value = event["Choices"][i]["Outcome"][0]["IncreaseValue"]
-		GlobalEvent.choice_one.outcome_negative_prompt = event["Choices"][i]["Outcome"][1]["Prompt"]
-		GlobalEvent.choice_one.outcome_negative_decrease_value = event["Choices"][i]["Outcome"][1]["DecreaseValue"]
+	#Load choice prompts
+	GlobalEvent.choice_one.choice_prompt = event["Choices"][0]["ChoicePrompt"]
+	GlobalEvent.choice_two.choice_prompt = event["Choices"][1]["ChoicePrompt"]
+	GlobalEvent.choice_three.choice_prompt = event["Choices"][2]["ChoicePrompt"]
+	GlobalEvent.choice_four.choice_prompt = event["Choices"][3]["ChoicePrompt"]
+	GlobalEvent.choice_five.choice_prompt = event["Choices"][4]["ChoicePrompt"]
+	
+	#Load choice types
+	GlobalEvent.choice_one.choice_type = event["Choices"][0]["ChoiceType"]
+	GlobalEvent.choice_two.choice_type = event["Choices"][1]["ChoiceType"]
+	GlobalEvent.choice_three.choice_type = event["Choices"][2]["ChoiceType"]
+	GlobalEvent.choice_four.choice_type = event["Choices"][3]["ChoiceType"]
+	GlobalEvent.choice_five.choice_type = event["Choices"][4]["ChoiceType"]
+	
+	#Load increase types
+	GlobalEvent.choice_one.increase_type = event["Choices"][0]["IncreaseType"]
+	GlobalEvent.choice_two.increase_type = event["Choices"][1]["IncreaseType"]
+	GlobalEvent.choice_three.increase_type = event["Choices"][2]["IncreaseType"]
+	GlobalEvent.choice_four.increase_type = event["Choices"][3]["IncreaseType"]
+	GlobalEvent.choice_five.increase_type = event["Choices"][4]["IncreaseType"]
+	
+	#Load decrease types
+	GlobalEvent.choice_one.decrease_type = event["Choices"][0]["DecreaseType"]
+	GlobalEvent.choice_two.decrease_type = event["Choices"][1]["DecreaseType"]
+	GlobalEvent.choice_three.decrease_type = event["Choices"][2]["DecreaseType"]
+	GlobalEvent.choice_four.decrease_type = event["Choices"][3]["DecreaseType"]
+	GlobalEvent.choice_five.decrease_type = event["Choices"][4]["DecreaseType"]
+	
+	#Load success chances
+	GlobalEvent.choice_one.success_chance = event["Choices"][0]["SuccessChance"]
+	GlobalEvent.choice_two.success_chance = event["Choices"][1]["SuccessChance"]
+	GlobalEvent.choice_three.success_chance = event["Choices"][2]["SuccessChance"]
+	GlobalEvent.choice_four.success_chance = event["Choices"][3]["SuccessChance"]
+	GlobalEvent.choice_five.success_chance = event["Choices"][4]["SuccessChance"]
+	
+	#Load skill modifiers
+	GlobalEvent.choice_one.skill_modifier = event["Choices"][0]["SkillModifier"]
+	GlobalEvent.choice_two.skill_modifier = event["Choices"][1]["SkillModifier"]
+	GlobalEvent.choice_three.skill_modifier = event["Choices"][2]["SkillModifier"]
+	GlobalEvent.choice_four.skill_modifier = event["Choices"][3]["SkillModifier"]
+	GlobalEvent.choice_five.skill_modifier = event["Choices"][4]["SkillModifier"]
+	
+	#Load positive prompts
+	GlobalEvent.choice_one.outcome_positive_prompt = event["Choices"][0]["Outcome"][0]["Prompt"]
+	GlobalEvent.choice_two.outcome_positive_prompt = event["Choices"][1]["Outcome"][0]["Prompt"]
+	GlobalEvent.choice_three.outcome_positive_prompt = event["Choices"][2]["Outcome"][0]["Prompt"]
+	GlobalEvent.choice_four.outcome_positive_prompt = event["Choices"][3]["Outcome"][0]["Prompt"]
+	GlobalEvent.choice_five.outcome_positive_prompt = event["Choices"][4]["Outcome"][0]["Prompt"]
+	
+	#Load positive increase values
+	GlobalEvent.choice_one.outcome_positive_increase_value = event["Choices"][0]["Outcome"][0]["IncreaseValue"]
+	GlobalEvent.choice_two.outcome_positive_increase_value = event["Choices"][1]["Outcome"][0]["IncreaseValue"]
+	GlobalEvent.choice_three.outcome_positive_increase_value = event["Choices"][2]["Outcome"][0]["IncreaseValue"]
+	GlobalEvent.choice_four.outcome_positive_increase_value = event["Choices"][3]["Outcome"][0]["IncreaseValue"]
+	GlobalEvent.choice_five.outcome_positive_increase_value = event["Choices"][4]["Outcome"][0]["IncreaseValue"]
+	
+	#Load negative prompts
+	GlobalEvent.choice_one.outcome_negative_prompt = event["Choices"][0]["Outcome"][1]["Prompt"]
+	GlobalEvent.choice_two.outcome_negative_prompt = event["Choices"][1]["Outcome"][1]["Prompt"]
+	GlobalEvent.choice_three.outcome_negative_prompt = event["Choices"][2]["Outcome"][1]["Prompt"]
+	GlobalEvent.choice_four.outcome_negative_prompt = event["Choices"][3]["Outcome"][1]["Prompt"]
+	GlobalEvent.choice_five.outcome_negative_prompt = event["Choices"][4]["Outcome"][1]["Prompt"]
+	
+	#Load negative decrease values
+	GlobalEvent.choice_one.outcome_negative_decrease_value = event["Choices"][0]["Outcome"][1]["DecreaseValue"]
+	GlobalEvent.choice_two.outcome_negative_decrease_value = event["Choices"][1]["Outcome"][1]["DecreaseValue"]
+	GlobalEvent.choice_three.outcome_negative_decrease_value = event["Choices"][2]["Outcome"][1]["DecreaseValue"]
+	GlobalEvent.choice_four.outcome_negative_decrease_value = event["Choices"][3]["Outcome"][1]["DecreaseValue"]
+	GlobalEvent.choice_five.outcome_negative_decrease_value = event["Choices"][4]["Outcome"][1]["DecreaseValue"]
 
 
 
